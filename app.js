5621c69c1d21fb4d473b407ebc7fef73a99cc5c3
Het0207 <panchalhet18@gmail.com>
2023-06-27 03:39:05 +0530

    bcrypt level of security

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 // const encrypt = require('mongoose-encryption'); // used a new package
-const md5 = require("md5");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const mongoose = require("mongoose");
 
 
 const app = express();
 
-// console.log(process.env.API_KEY);
+// console.log(process.env.API_KEY); 
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -46,34 +48,43 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-    newUser.save();
-    res.render("secrets");
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save();
+        res.render("secrets");
+    });
+
 })
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
+
     main().catch((err) => console.log(err));
 
     async function main() {
-        const foundUser = await User.findOne({email:username});
-        if(!foundUser){
+        const foundUser = await User.findOne({ email: username });
+        if (!foundUser) {
             console.log("User not found");
         }
-        else{
-            if(foundUser.password === md5(password)){ // hash value of two same values will be same
-                res.render("secrets");
-            }
+        else {
+            // if(foundUser.password === (password))// hash value of two same values will be same
+            bcrypt.compare(password,foundUser.password, function(err,result){
+                if(result === true){    
+                    res.render("secrets");
+                }
+            })
+            
+
         }
     }
 
 
-})
+});
 
 app.listen(3000, function (req, res) {
     console.log("Server started at 3000");
-})
+});
